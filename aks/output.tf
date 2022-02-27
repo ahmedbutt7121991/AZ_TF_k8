e117f6a3c19571d194e522d59664fd5c6b2ebbfc
AHMED BUTT (Author) <ahmed.butt91@gmail.com>
2022-02-27 02:45:39 +0000

    Error resolved

--- aks/output.tf
+++ aks/output.tf
@@ -1,9 +1,9 @@
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.kube_cluster_1.kube_config.0.client_certificate
+  value = azurerm_kubernetes_cluster.kc_az_k8.kube_config.0.client_certificate
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.kube_cluster_1.kube_config_raw
+  value = azurerm_kubernetes_cluster.kc_az_k8.kube_config_raw
 
   sensitive = true
 }
--- main.tf
+++ main.tf
@@ -7,23 +7,23 @@ terraform {
   }
 }
 
-module "rg_mod" {
+module "arg_mod" {
   source = "./rg"
-  rg_id = module.rg_mod.rg_id
-  rg_name = module.rg_mod.rg_name
-  rg_location = module.rg_mod.rg_location
+  rg_id = module.arg_mod.rg_id
+  rg_name = module.arg_mod.rg_name
+  rg_location = module.arg_mod.rg_location
 }
 
 module "aks_mod" {
   source = "./aks"
-  rg_name = module.rg_mod.rg_name
-  rg_location = module.rg_mod.rg_location
-  rc_id = module.rc_mod.rc_id
+  rg_name = module.arg_mod.rg_name
+  rg_location = module.arg_mod.rg_location
+  rc_id = module.rc_mod.arc_id
 }
 
 module "acr_mod" {
   source = "./acr"
-  rc_id = module.rc_mod.rc_id
-  rg_name = module.rg_mod.rg_name
-  rg_location = module.rg_mod.rg_location
+  rc_id = module.arc_mod.rc_id
+  rg_name = module.arg_mod.rg_name
+  rg_location = module.arg_mod.rg_location
 }
