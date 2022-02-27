fa23767ec3ae6b0184266370ea858ba8caf23a55
AHMED BUTT (Author) <ahmed.butt91@gmail.com>
2022-02-27 02:11:03 +0000

    "Resource Group Module Completed"

--- rg/output.tf
+++ rg/output.tf
@@ -1,7 +1,7 @@
 output "rg_id" {
-  value = azurerm_resource_group.azure_rg_kube.id
+  value = azurerm_resource_group.rg_az_k8.id
 }
 
 output "rg_name" {
-  value = azurerm_resource_group.azure_rg_kube.name
+  value = azurerm_resource_group.rg_az_k8.name
 }
---
+++ rg/vars.tf
@@ -0,0 +1,8 @@
+variable rg_id {}
+variable rg_name {}
+variable name {
+  default = "resourcegroup_az_k8"
+}
+variable location {
+  default = "West Europe"
+}
